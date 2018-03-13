a960c486ac59099d970693f0dff8dfba428dffed
Adam Paquette <acp263@nau.edu>
2018-03-13 10:19:24 -0700

    Updates to function names, added basic admin login functionality.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
-from wtforms import StringField, Form
-from wtforms.validators import AnyOf, DataRequired
+from wtforms import TextField, StringField, Form
+from wtforms.validators import AnyOf, DataRequired, required
 from wtforms.widgets import TextArea
 
 class PageForm(Form):
@@ -14,3 +14,7 @@ class PageForm(Form):
     page_content = StringField(u"Content",
                                [DataRequired(message = "No webpage content given.")],
                                widget = TextArea())
+
+class LoginForm(Form):
+    admin_name = TextField(u'Admin Name', [required()])
+    admin_password = TextField(u'Admin Password', [required()])
--- pgm.py
+++ pgm.py
@@ -1,24 +1,51 @@
 import os
 import sys
+from functools import wraps
 from datetime import datetime
 
 from flask import Flask, abort, flash, redirect, render_template, request, url_for, Response
 from pymongo import MongoClient
 from jinja2 import Markup
 
-from forms import PageForm
+from forms import PageForm, LoginForm
 
 if sys.version_info[0] == 3:
   from urllib.request import urlopen
 else:
   from urllib import urlopen
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('db', 27017)
 db = client.pgmdb
 
-WSApp = Flask(__name__, instance_relative_config=True)
+admin = {'admin_name': '', 'admin_password': ''}
+
+WSApp = Flask(__name__, instance_relative_config=False)
 WSApp.debug=True
-# WSApp.config.from_pyfile('config.py')
+WSApp.config.from_pyfile('config.py')
+print(WSApp.config)
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if admin['admin_name'] != WSApp.config['ADMIN_NAME'] and \
+           admin['admin_password']!= WSApp.config['ADMIN_PASSWORD']:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@WSApp.route('/login', methods = ['GET', 'POST'])
+def login():
+    menu = Markup(db.static.find_one({'name': 'menu'})['content'])
+    form = LoginForm(request.form)
+
+    if request.method == 'POST':
+        admin['admin_name'] = form.admin_name.data
+        admin['admin_password'] = form.admin_password.data
+
+        return redirect(url_for('edit_pages'))
+
+    return render_template('login_template.html', menu = menu, form = form)
+
 
 @WSApp.route('/', methods = ['GET'])
 def index_handler():
@@ -51,7 +78,8 @@ def map_all_handler():
     return render_template('map_template.html', body = 'All', menu = menu)
 
 @WSApp.route('/edit', methods = ['GET', 'POST'])
-def editPage():
+@admin_required
+def edit_page():
     form = PageForm(request.form)
     name = request.args['page']
     doc = db.static.find_one({'name': name})
@@ -64,7 +92,7 @@ def editPage():
         db.static.update({'name':name}, {"$set": {'content': new_content,
                                                   'title': new_title,
                                                   'name': new_name}})
-        return redirect(url_for('editPages'))
+        return redirect(url_for('edit_pages'))
 
     file_title = Markup(doc['title'])
     file_markup = Markup(doc['content'])
@@ -74,13 +102,15 @@ def editPage():
     return render_template('edit.html', form = form, menu = menu)
 
 @WSApp.route('/remove', methods = ['POST'])
-def removePage():
+@admin_required
+def remove_page():
     name =  request.form['page'];
     doc = db.static.delete_one({'name': name})
-    return redirect(url_for('editPages'))
+    return redirect(url_for('edit_pages'))
 
 @WSApp.route('/admin/new', methods = ['GET', 'POST'])
-def createPage():
+@admin_required
+def create_page():
     form = PageForm(request.form)
     menu = Markup(db.static.find_one({'name': 'menu'})['content'])
 
@@ -92,22 +122,23 @@ def createPage():
                           'content': content,
                           'title': title,
                           'type': 'page'})
-        return redirect(url_for('editPages'))
+        return redirect(url_for('edit_pages'))
 
     return render_template('edit.html', form = form, menu = menu)
 
 @WSApp.route('/admin/pages', methods = ['GET'])
-def editPages():
+@admin_required
+def edit_pages():
     menu = Markup(db.static.find_one({'name': 'menu'})['content'])
     entries = [entry['name'] for entry in db.static.find({})]
     return render_template('pages.html', menu = menu, entries = entries)
 
 @WSApp.route("/Project/view/<id>")
-def projectHandler(id):
+def project_handler(id):
     return render_template('project.html', id = id, header_dict = header_dict)
 
 @WSApp.route("/datasets/<environment>/<dataset>/<target>/<protocol>/<ident>")
-def jsonDatasetId(environment, dataset, target, protocol, ident):
+def json_dataset_id(environment, dataset, target, protocol, ident):
     url = ''
     if environment == 'dev':
         url = 'http://astrocloud-dev.wr.usgs.gov/dataset/data/'
@@ -117,13 +148,12 @@ def jsonDatasetId(environment, dataset, target, protocol, ident):
     url += '?request=getFeature&service=WFS&version=1.1.0&outputformat=application/json'
     if ident is not None:
         url += '&id=' + ident
-        print(url)
     response = urlopen(url)
-    return Response(response.read(),status=200,mimetype='application/json')
+    return Response(response.read(), status=200, mimetype='application/json')
 
 @WSApp.route("/datasets/<environment>/<dataset>/<target>/<protocol>")
-def jsonDatasetTarget(environment, dataset, target, protocol):
-    return jsonDatasetId (environment, dataset, target, protocol, None)
+def json_dataset_target(environment, dataset, target, protocol):
+    return json_dataset_id (environment, dataset, target, protocol, None)
 
 if __name__ == "__main__":
     # @TODO probably need to remove host arg for production code, but it's
